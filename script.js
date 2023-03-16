b1871816ac87081e770a5a8af96d630383bbc556
McNugg1es <willejohansson3810@gmail.com>
2023-03-16 08:49:02 +0100

    1

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+function buttonClick() {
+    alert("Hello")
+
+}
+
+function ButtonClick() {
+    var container = document.getElementById("container")
+    console.log(container.children)
+ 
+}
+
+function BUTTONClick() {
+    document.getElementById("main-title").textContent = document.getElementById("A").value
+}
